345600c869fc3943a80c024f253df2aef4d594f1
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-08-22 17:13:13 -0300

    feat: criação de server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const serverPort = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res.status(200)
+    .send({ message: "Hello, word!"}) 
+})
+
+app.listen(serverPort, () => {
+    console.log(`🃏 Server started on http://localhost:${serverPort}`);
+})
