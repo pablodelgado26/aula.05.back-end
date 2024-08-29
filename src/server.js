199e032ed0700f9b4bb89ab5ab04e9d2f4a7b417
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-08-29 10:50:06 -0300

    feat: Adicionando novas requisições

--- src/server.js
+++ src/server.js
@@ -8,9 +8,56 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "verde"
+    }, 
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Alegria",
+        cor: "Amarelo"
+    }
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Brian O'Conner",
+        vivo: false,
+        studio: " Universal Studios "
+    },
+    {
+        id: 100,
+        nome: "Godzilla",
+        vivo: true,
+        studio: " TriStar Pictures "
+    },
+    {
+        id: 100,
+        nome: "Goku",
+        vivo: true,
+        studio: " Toei Animation "
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200)
     .send({ message: "Hello, word!"}) 
+})
+app.get("/personagens", (req, res) => {
+    return res.status(200)
+    .send( personagens ) 
+})
+app.get("/emocoes", (req, res) => {
+    return res.status(200)
+    .send( emocoes ) 
 })
 app.get("/2tds2", (req, res) => {
     return res.status(200)
