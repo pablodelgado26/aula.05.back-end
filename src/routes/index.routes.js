440cec64906c08b291caed53ebda0c6c3af46036
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2024-09-26 16:59:45 -0300

    feat: criando rotas para personagens

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
 import emocoesRoutes from "./emocoes.routes.js";
+import personagensRoutes from "./personagens.routes.js";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-    return res.status(200)
-    .send({ message: "Hello, word!"}) 
-});
-routes.get("/2tds2", (req, res) => {
-    return res.status(200)
-    .send({ message: "Hello, word!"}) 
-});
-
 routes.use("/emocoes", emocoesRoutes);
+routes.use("/personagens", personagensRoutes);
 
 export default routes;
--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -4,26 +4,57 @@ const personagensRoutes = Router();
 
 let personagens = [
     {
-        id: 100,
+        id: 1,
         nome: "Brian O'Conner",
         vivo: false,
         studio: " Universal Studios "
     },
     {
-        id: 100,
+        id: 2,
         nome: "Godzilla",
         vivo: true,
         studio: " TriStar Pictures "
     },
     {
-        id: 100,
+        id: 3,
         nome: "Goku",
         vivo: true,
         studio: " Toei Animation "
     }
 ];
 
-personagensRoutes.get("/personagens", (req, res) => {
+personagensRoutes.get("/", (req, res) => {
     return res.status(200)
     .send( personagens ) 
 });
+
+// Criando novo personagem
+personagensRoutes.post("/", (req, res) => {
+    const{ nome, studio, vivo } = req.body;
+    const novoPersonagem = {
+        id: personagens.length + 1,
+        nome: nome,
+        studio: studio,
+        vivo: vivo
+    }
+    personagens.push(novoPersonagem)
+    return res.status(201)
+    .send( novoPersonagem ) 
+});
+
+personagensRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const personagem =  personagens.find((person) => person.id == id);
+    if(!personagem){
+        return res.status(404).send({
+            message: "Personagem não encontrado!",
+        });
+    }
+    personagens = personagens.filter((person) => person.id != id);
+    return res.status(200).send({
+        message: "Personagem deletado!",
+        personagem,
+    });
+});
+
+export default personagensRoutes;
